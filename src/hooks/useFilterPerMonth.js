7f3d8c413f77250df2ba38c9e59611a7db069fda
samfromaway@gmail.com <email@example.com>
2020-06-15 16:13:49 -0500

    works

--- src/hooks/useFilterPerMonth.js
+++ src/hooks/useFilterPerMonth.js
@@ -1,157 +1,59 @@
 import { useEffect, useState } from 'react';
-import { CURRENT_YEAR } from '../constants';
 
 export const useFilterPerMonth = (transactions, categories) => {
   const [chartData, setChartData] = useState([]);
+  const categoryValues = categories.map((category) => category.value);
 
   useEffect(() => {
-    function filterByMonth(from, to) {
-      const total = transactions.filter(
-        (transaction) =>
-          transaction.date >= CURRENT_YEAR + from &&
-          transaction.date <= CURRENT_YEAR + to
+    const categoriesInMonth = (array, filterCategory, filterMonth) => {
+      return array.filter(
+        (item) =>
+          item.category === filterCategory &&
+          new Date(item.date).getMonth() === filterMonth
       );
-
-      return total;
-    }
-
-    const sumByCategoryByMonth = (month, monthData) => {
-      const hteProducts = [0];
-      const hteServices = [0];
-      const comissions = [0];
-      const devWorld = [0];
-      const youtube = [0];
-
-      monthData.forEach((e) => {
-        switch (e.category) {
-          case 'hteProducts':
-            hteProducts.push(e.chfAmount);
-            break;
-          case 'hteServices':
-            hteServices.push(e.chfAmount);
-            break;
-          case 'comissions':
-            comissions.push(e.chfAmount);
-            break;
-          case 'devWorld':
-            devWorld.push(e.chfAmount);
-            break;
-          case 'youtube':
-            youtube.push(e.chfAmount);
-            break;
-
-          default:
-            alert('error in generating chart');
-          //error can be deleted in production
-        }
-      });
-
-      /////////////////////////////////////////
-      var data = [
-        {
-          id: 1,
-          term_id: 5,
-          type: 'car',
-        },
-        {
-          id: 2,
-          term_id: 3,
-          type: 'bike',
-        },
-        {
-          id: 3,
-          term_id: 6,
-          type: 'car',
-        },
-      ];
-
-      var result = data.filter(function (v, i) {
-        return (v['term_id'] == 5 || v['term_id'] == 6) && v.type == 'car';
-      });
-
-      console.log(result);
-
-      //  https://medium.com/better-programming/creating-a-multi-filter-function-to-filter-out-multiple-attributes-javascript-react-rails-5aad8e272142
-
-      ///////////////////////////////////////////
-
-      return {
-        month: month,
-        hteProducts: hteProducts.reduce((acc, item) => (acc += item), 0),
-        hteServices: hteServices.reduce((acc, item) => (acc += item), 0),
-        comissions: comissions.reduce((acc, item) => (acc += item), 0),
-        devWorld: devWorld.reduce((acc, item) => (acc += item), 0),
-        youtube: youtube.reduce((acc, item) => (acc += item), 0),
-      };
     };
 
-    const janTotal = sumByCategoryByMonth(
-      'Jan',
-      filterByMonth('-01-01', '-01-31')
-    );
-    const febTotal = sumByCategoryByMonth(
-      'Feb',
-      filterByMonth('-02-01', '-02-31')
-    );
+    const categorySum = (array) => {
+      const sum = array
+        .map((e) => e.amount)
+        .reduce((acc, item) => (acc += item), 0);
+      return sum;
+    };
 
-    const marTotal = sumByCategoryByMonth(
-      'Mar',
-      filterByMonth('-03-01', '-03-31')
-    );
-    const aprTotal = sumByCategoryByMonth(
-      'Apr',
-      filterByMonth('-04-01', '-04-31')
-    );
-    const mayTotal = sumByCategoryByMonth(
-      'May',
-      filterByMonth('-05-01', '-05-31')
-    );
-    const junTotal = sumByCategoryByMonth(
-      'Jun',
-      filterByMonth('-06-01', '-06-31')
-    );
-    const julTotal = sumByCategoryByMonth(
-      'Jul',
-      filterByMonth('-07-01', '-07-31')
-    );
-    const augTotal = sumByCategoryByMonth(
-      'Aug',
-      filterByMonth('-08-01', '-08-31')
-    );
-    const sepTotal = sumByCategoryByMonth(
-      'Sep',
-      filterByMonth('-09-01', '-09-31')
-    );
-    const octTotal = sumByCategoryByMonth(
-      'Oct',
-      filterByMonth('-10-01', '-10-31')
-    );
-    const novTotal = sumByCategoryByMonth(
-      'Nov',
-      filterByMonth('-11-01', '-11-31')
-    );
-    const decTotal = sumByCategoryByMonth(
-      'Dec',
-      filterByMonth('-12-01', '-12-31')
-    );
+    const categoryByMonth = (monthString, monthNr) => {
+      const total = { month: monthString };
 
+      categoryValues
+        .map((e) => categoriesInMonth(transactions, e, monthNr))
+        .forEach((e) => {
+          if (e.length) {
+            total[e[0].category] = categorySum(e);
+          }
+        });
+      return total;
+    };
+
+    //
+    //
+    //
+    //
     const data = [
-      janTotal,
-      febTotal,
-      marTotal,
-      aprTotal,
-      mayTotal,
-      junTotal,
-      julTotal,
-      augTotal,
-      sepTotal,
-      octTotal,
-      novTotal,
-      decTotal,
+      categoryByMonth('Jan', 0),
+      categoryByMonth('Feb', 1),
+      categoryByMonth('Mar', 2),
+      categoryByMonth('Apr', 3),
+      categoryByMonth('May', 4),
+      categoryByMonth('Jun', 5),
+      categoryByMonth('Jul', 6),
+      categoryByMonth('Aug', 7),
+      categoryByMonth('Sep', 8),
+      categoryByMonth('Oct', 9),
+      categoryByMonth('Nov', 10),
+      categoryByMonth('Dec', 11),
     ];
+    console.log(data);
 
     setChartData(data);
   }, [transactions]);
-
   return chartData;
 };
