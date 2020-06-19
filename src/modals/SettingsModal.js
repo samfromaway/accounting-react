1baf54d0e0d7e26c4a12a6e5f762ad8343b9fe96
samfromaway@gmail.com <email@example.com>
2020-06-19 08:40:40 -0500

    little edits

--- src/modals/SettingsModal.js
+++ src/modals/SettingsModal.js
@@ -63,6 +63,7 @@ const SettingsModal = ({ openSettingsModal, handleCloseSettingsModal }) => {
       <Fade in={openSettingsModal}>
         <Paper className={classes.paper}>
           <Typography variant='h5'>Settings</Typography>
+          <Divider style={{ margin: '0.5rem 0 1rem 0 ' }} />
           <SettingsTabs
             expensesTransactions={expensesTransactions}
             expensesCategories={expensesCategories}
